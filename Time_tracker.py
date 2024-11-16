c13308e1162bea3af76a98dfbd92b3e6303105ac
Bita <87811454+Bita404@users.noreply.github.com>
2024-11-16 15:53:39 +0330

    Add files via upload

--- Time_tracker.py
+++ Time_tracker.py
@@ -15,4 +15,18 @@ class Project:
         pass   
 
 class Task(Project): 
-    pass 
+    def __init__(self):
+        super().__init__()
+        pass
+    
+    def Add_task():
+        pass
+    
+    def Edit_task():
+        pass
+    
+    def Remove_task():
+        pass
+    
+    def Mark_task():
+        pass
