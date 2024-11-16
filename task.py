89a154e63a60db87c7efa1b073940510946994bc
Bita <87811454+Bita404@users.noreply.github.com>
2024-11-17 00:18:21 +0330

    Add files via upload

--- task.py
+++ task.py
@@ -2,11 +2,19 @@ from project import Project
 import datetime 
 
 class Task(Project): 
-    def __init__(self):
+    def __init__(self , name , ID , description , start_time , end_time, duration, pro_association ,  status=False ):
         super().__init__()
-        pass
+        self.name = name 
+        self.ID = ID
+        ID = 1000
+        self.description = description
+        self.status = status 
+        self.start_time = start_time
+        self.end_time = end_time 
+        self.duration = duration
+        self.pro_association = pro_association  
     
-    def Add_task():
+    def Add_task(self):
         pass
     
     def Edit_task():
