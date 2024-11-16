6bd54a90bff231fa565a817dd8126e1ddc0e0c73
Bita <87811454+Bita404@users.noreply.github.com>
2024-11-16 18:29:47 +0330

    Add files via upload

--- Time_tracker/project.py
+++ Time_tracker/project.py
@@ -1,16 +1,27 @@
+
+
 class Project:
-    def __init__(self , name , Tasks):
-      self.name =name
-      self.tasks = Tasks
+    def __init__(self):
+      self.projects = {}
     
-    def Add_pro():
-        pass
+    def Add_pro(self , new_pro ):
+        if new_pro in self.projects:
+            raise ValueError(f" project '{new_pro}' Already exist ! ! ")
+        self.projects[new_pro] = [] # empty space for the new project tasks
+                     
+    def Edit_pro(self,  old_name , new_name):
+        if old_name not in self.projects :
+             raise ValueError(f"'{old_name}' Invalid project name ! ")]
+        if new_name in self.projects :
+              raise ValueError(f" '{new_name}' Already exist  !!! try another name")
     
-    def Edit_pro():
-        pass
+    def Remove_pro(self , rem_pro):
+        if rem_pro in self.projects :
+            raise ValueError(f"'{rem_pro}' Invalid project ! ! ")
+        del self.projects[rem_pro]
     
-    def Remove_pro():
-        pass
-    
-    def Display_pro():
-        pass   
+    def Display_pro(self):
+        if not self.projects :
+            print(" No Projects Found ! ! ")
+        else :
+              
