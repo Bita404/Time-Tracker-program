cd1a8365753348941c4e3141751590018702aa50
Bita <87811454+Bita404@users.noreply.github.com>
2024-11-16 18:45:01 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+from project import Project
+from task import Task
+
+def main():
+    pass
---
+++ task.py
@@ -0,0 +1,19 @@
+from project import Project 
+
+class Task(Project): 
+    def __init__(self):
+        super().__init__()
+        pass
+    
+    def Add_task():
+        pass
+    
+    def Edit_task():
+        pass
+    
+    def Remove_task():
+        pass
+    
+    def Mark_task():
+        pass
+    
