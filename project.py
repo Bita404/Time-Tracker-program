edc27935aba77acbfca98447e19025dc7201e1e3
Bita <87811454+Bita404@users.noreply.github.com>
2024-11-20 02:34:45 +0330

    Add files via upload

--- project.py
+++ project.py
@@ -9,35 +9,34 @@ class Project :
         else : 
             raise ValueError("This project Already exists ! ")   
       
-    
     #def Add_pro(self , new_pro ):
     #    if new_pro in self.projects:
     #        raise ValueError(f" project '{new_pro}' Already exist ! ! ")
     #    self.projects[new_pro] = [] # empty space for the new project tasks
                      
     def Edit_pro(self,  old_name , new_name):
-        if old_name not in self.projects :
+        if old_name not in Project.name_List :
              raise ValueError(f"'{old_name}' Invalid project name ! ")
-        if new_name in self.projects :
+        if new_name in Project.name_List :
               raise ValueError(f" '{new_name}' Already exist  !!! try another name")
     
     def Remove_pro(self , rem_pro):
-        if rem_pro in self.projects : 
+        if rem_pro in Project.name_List : 
             raise ValueError(f"'{rem_pro}' Invalid project ! ! ")
-        del self.projects[rem_pro]
+        del Project.name_List[rem_pro]
     
-    def Display_pro(self):
-        if not self.projects :
-            print(" No Projects Found ! ! ")
-        else :
-           print(f"{self.projects}")    
+    def Display_pro():
+           print(f"{Project.name_List}")    
              
     def __str__(self):
         return f"project : {self.project_name}"       
+
 
 p1 =Project("cake")
 print(p1)
 p2 =Project("CLI")
 print(p2)
-p3=Project("cake")
-print(p3)
+#p3=Project("cake")
+#print(p3)
+
+Project.Display_pro()
