f018f8ecdc455a01050a3d4620e29f97e4de0d83
Bita <87811454+Bita404@users.noreply.github.com>
2024-11-23 02:04:40 +0330

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from project import *
+import sys
 from task import *
 
 def main():
@@ -7,12 +8,12 @@ def main():
         print("1. Add Project")
         print("2. Add Task")
         print("3. Edit Project")
-        print("4. Edit project")
+        print("4. Edit Task")
         print("5. Remove Project or Task")  # it has 2 options
         print("6. Mark a Task as Done ")
         print("7. Display")  # it has 2 options
         print("8. Search")   # 2 options 
-        print("9. Save ")  # data export
+        print("9. Save Data ")  # data export
         print("10. Exit")
         
         
@@ -20,3 +21,24 @@ def main():
         
         if answer =="1":
             pass
+        elif answer =="2":
+            pass
+        elif answer =="3":
+            pass
+        elif answer =="4":
+            pass
+        elif answer =="5":
+            pass
+        elif answer =="6":
+            pass
+        elif answer =="7":
+            pass
+        elif answer=="8":
+            pass
+        elif answer=="9":
+            pass
+        elif answer=="10":
+            print("ok Goodbye (*＾▽＾)／  Exiting ...")
+            break
+        else :
+            print("Invalid choice ! ! ! Please choose a number (1-10) :3 ")
